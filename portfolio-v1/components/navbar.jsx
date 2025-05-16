3933ebfc3400a550c5ee18d5caf5a5e734af4e82
Vincent <mr.martinezpuentes@gmail.com>
2025-05-16 12:45:02 -0700

    Soft Changes

--- portfolio-v1/components/navbar.jsx
+++ portfolio-v1/components/navbar.jsx
@@ -5,22 +5,7 @@ export default function Navigation(){
 
     return (
 
-        <Navbar isBordered maxWidth="full" className=""       classNames={{
-          item: [
-            "flex",
-            "relative",
-            "h-full",
-            "items-center",
-            "data-[active=true]:after:content-['']",
-            "data-[active=true]:after:absolute",
-            "data-[active=true]:after:bottom-0",
-            "data-[active=true]:after:left-0",
-            "data-[active=true]:after:right-0",
-            "data-[active=true]:after:h-[2px]",
-            "data-[active=true]:after:rounded-[2px]",
-            "data-[active=true]:after:bg-primary",
-          ],
-        }}>
+        <Navbar isBordered maxWidth="full" className="">
         <NavbarContent className="w-full" data-justify="center">
           <NavbarItem>
             <Link className="text-color-black hover:text-color-blue" href="/">
