7885d6d97c5e63a54aa40c39297234c5b60c6141
Vincent <vmart044@ucr.edu>
2025-05-30 14:27:38 -0700

    Updates to about

--- portfolio-v1/app/about/page.tsx
+++ portfolio-v1/app/about/page.tsx
@@ -25,73 +25,75 @@ export default function Home() {
 
   return (
     <>
-      <main className="min-h-screen pl-40 pr-40 pt-10 absolute inset-0 bg-gradient-to-br from-emerald-200/70 to-sky-300/70">
+      <main className="flex relative min-h-screen">
+        <div className="absolute inset-0 bg-[url('/LW002.jpg')] bg-cover bg-center bg-no-repeat">
+          <div className="absolute inset-0 bg-gradient-to-br from-emerald-200/70 to-sky-300/70">
+            <div className="flex-col pl-40 pr-40 pt-10">
+                <div className="flex">
+                  <div className="flex-col">
 
-        <div className="flex-col">
-            <div className="flex">
-              <div className="flex-col">
+                    <Card className="pb-5 mb-5 hover:-translate-y-1 hover:scale-30">
+                      <CardBody>
+                          <h2 className=" mt-4 ml-2 text-3xl italic">About me</h2>
+                          <p className="text-start mt-4 ml-2 text-xl">
+                            My name is Vincent Martinez and I am a software engineer with a passion for creating beautiful and functional web applications. I have experience in various programming languages and frameworks, and I am always eager to learn new technologies.
+                          </p>
+                        </CardBody>
+                    </Card>
 
-                <Card className="pb-5 mb-5 hover:-translate-y-1 hover:scale-30">
-                  <CardBody>
-                      <h2 className=" mt-4 ml-2 text-3xl italic">About me</h2>
-                      <p className="text-start mt-4 ml-2 text-xl">
-                        My name is Vincent Martinez and I am a software engineer with a passion for creating beautiful and functional web applications. I have experience in various programming languages and frameworks, and I am always eager to learn new technologies.
-                      </p>
-                    </CardBody>
-                </Card>
+                    <Card className="pb-5 mb-5 hover:-translate-y-1 hover:scale-30">
+                      <CardBody>
+                          <h2 className=" mt-4 ml-2 text-3xl italic">Interests</h2>
+                          <p className="text-start mt-4 ml-2 text-xl">
+                          I am interested in networks, web development, and devops. 
+                          </p>
+                      </CardBody>
+                    </Card>
 
-                <Card className="pb-5 mb-5 hover:-translate-y-1 hover:scale-30">
-                  <CardBody>
-                      <h2 className=" mt-4 ml-2 text-3xl italic">Interests</h2>
-                      <p className="text-start mt-4 ml-2 text-xl">
-                      I am interested in networks, web development, and devops. 
-                      </p>
-                  </CardBody>
-                </Card>
+                    <Card className="pb-5 hover:-translate-y-1 hover:scale-30">
+                      <CardBody>
+                          <h2 className=" mt-4 ml-2 text-3xl italic">Hobbies</h2>
+                          <p className="text-start mt-4 ml-2 text-xl">
+                            Some of my hobbies include playing games, exercising frequently, and learning musical instruments. I am currently making a small video game in my freetime with the use of Unity and I hope to incorporate some music that I create for it as the soundtrack!
+                          </p>
+                      </CardBody>
+                    </Card>
+                  </div> 
+              
+                </div>
+{/* 
+                <div 
+                    id="carousel-scroll"
+                    className="flex justify-center overflow-x-auto whitespace-nowrap py-8">
+                      <InfiniteScroll
+                        dataLength={carouselImages.length}
+                        next={fetchData}
+                        hasMore={hasMore}
+                        loader={<h4>Loading...</h4>}
+                        endMessage={
+                          <p style={{ textAlign: 'center' }}>
+                            <b>Yay! You have seen it all</b>
+                          </p>
+                        }
+                        scrollableTarget="carousel-scroll"
+                        className="flex flex-row gap-4">
 
-                <Card className="pb-5 hover:-translate-y-1 hover:scale-30">
-                  <CardBody>
-                      <h2 className=" mt-4 ml-2 text-3xl italic">Hobbies</h2>
-                      <p className="text-start mt-4 ml-2 text-xl">
-                        Some of my hobbies include playing games, exercising frequently, and learning musical instruments. I am currently making a small video game in my freetime with the use of Unity and I hope to incorporate some music that I create for it as the soundtrack!
-                      </p>
-                  </CardBody>
-                </Card>
-              </div> 
-          
-            </div>
+                          {carouselImages.map((image, idx) => (
+                            <Image
+                            key={image + idx}
+                            src={image}
+                            alt="Vincent Martinez"
+                            width={280}
+                            height={360}
+                            className="rounded-lg inline-block"
+                          />
+                      ))}
+                    </InfiniteScroll>
+                  </div> */}
 
-            <div 
-                id="carousel-scroll"
-                className="flex justify-center overflow-x-auto whitespace-nowrap py-8">
-                  <InfiniteScroll
-                    dataLength={carouselImages.length}
-                    next={fetchData}
-                    hasMore={hasMore}
-                    loader={<h4>Loading...</h4>}
-                    endMessage={
-                      <p style={{ textAlign: 'center' }}>
-                        <b>Yay! You have seen it all</b>
-                      </p>
-                    }
-                    scrollableTarget="carousel-scroll"
-                    className="flex flex-row gap-4">
-
-                      {carouselImages.map((image, idx) => (
-                        <Image
-                        key={image + idx}
-                        src={image}
-                        alt="Vincent Martinez"
-                        width={280}
-                        height={360}
-                        className="rounded-lg inline-block"
-                      />
-                  ))}
-                </InfiniteScroll>
               </div>
-
+            </div>
           </div>
-
         </main>
     </>
   );
