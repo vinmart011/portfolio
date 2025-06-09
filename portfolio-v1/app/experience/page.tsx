e507c2108afa1c4bfbabb66d952938d5b10bb478
Vincent <mr.martinezpuentes@gmail.com>
2025-06-09 12:18:46 -0700

    Adding pdf and placeholder download on about

--- portfolio-v1/app/experience/page.tsx
+++ portfolio-v1/app/experience/page.tsx
@@ -1,95 +1,71 @@
 'use client'
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import { Card, CardBody, Button } from "@heroui/react";
+import { useRef, useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Card, CardBody, Button, Image, Avatar, Divider } from "@heroui/react";
+import DownloadSVG from "@/components/downloadsvg";
 
 export default function Home() {
-  const phrases = [
-    "Hi, I'm", //English
-    "Hola, soy", //Spanish
-    "Bonjour, je suis", //French
-    "Ciao, sono", //Italian
-    "Hallo, ich bin", //German
-    "こんにちは、私は", //Japanese
-    "안녕하세요, 저는", //Korean
-  ];
-  const phraselang = [
-    "(US)",
-    "(ES)",
-    "(FR)",
-    "(IT)",
-    "(DE)",
-    "(JP)",
-    "(KP)"
-  ]
-
-  const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
-
-  //cycle through phrases every 5 seconds
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentPhraseIndex((prevIndex) => (prevIndex + 1) % phrases.length);
-    }, 5000);
-
-    return () => clearInterval(intervalId);
-  }, []);
+  const [carouselImages, setCarouselImages] = useState([
+    "/portrait.jpeg",
+    "/portrait2.png",
+    "/portrait3.png",
+  ]);
+  const [hasMore, setHasMore] = useState(true);
+  const fetchData = () => {
+    setTimeout(() => {
+      const newImages = [
+        "/portrait.jpeg",
+        "/portrait2.png",
+        "/portrait3.png",
+      ].map((img, idx) => `${img}?v=${carouselImages.length + idx}`);
+      setCarouselImages((prev) => [...prev, ...newImages]);
+    }, 1000);
+  };
 
   return (
     <>
-      <main className="flex flex-col min-h-screen">
-        <div className="flex-grow mt-60">
-          <h1 className="text-center text-5xl">
-            <span className="text-sm">{phraselang[currentPhraseIndex]}</span>{phrases[currentPhraseIndex]} <strong className="text-blue-500">Vincent Martinez</strong>
-          </h1>
-        </div>
+      <main className="w-full h-full bg-[url('/bg.png')] bg-cover bg-center bg-no-repeat p-4 md:p-8">
+        <div className="max-w-7xl mx-auto flex flex-col lg:flex-row gap-8 items-start pt-8 px-4">
 
-        <footer className="flex gap-6 flex-wrap items-center justify-center p-4 bg-gray-800 text-white">
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
+          <div className="flex-1">
+            <h2 className="mt-4 text-3xl italic text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-pink-600 font-proxima font-bold">
+              About
+            </h2>
+            <Divider className="h-[2px] w-[40%] bg-cyan-600" />
+            <p className="mt-4 text-lg md:text-xl font-proxima">
+              My name is Vincent Martinez and I am a software engineer with a Bachelors of Science in Computer Science from the University of California, Riverside.
+            </p>
+
+            <h2 className="mt-8 text-3xl italic text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-pink-600 font-proxima font-bold">
+              Interests
+            </h2>
+            <Divider className="h-[3px] w-[40%] bg-cyan-600" />
+            <p className="mt-4 text-lg md:text-xl font-proxima">
+              I love writing code that expresses my creativity and my passion for technology. To that extent, I enjoy making websites, using C++, and exploring what it takes to make a video game.
+            </p>
+
+            <h2 className="mt-8 text-3xl italic text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 to-pink-600 font-proxima font-bold">
+              Hobbies
+            </h2>
+            <Divider className="h-[2px] w-[40%] bg-cyan-600" />
+            <p className="mt-4 text-lg md:text-xl font-proxima">
+              When I'm not working on code, I enjoy exercising, learning new ways to cook, and figuring out how to play guitar. I would love to travel more often and experience new cultures as well!
+            </p>
+          </div>
+
+          <div className="flex items-center justify-center w-full max-w-md mx-auto">
+            <div className="relative w-full pb-20 group">
+              <Card className="bg-gradient-to-br from-cyan-500 to-pink-600">
+                <CardBody className="flex items-center transition-opacity duration-200">
+                  <div className="flex absolute items-center justify-center w-[50%] h-[50%]">
+                    <Button href="resume.pdf" endContent={<DownloadSVG />} className="w-full h-[20%] group-hover:opacity-100 translate-y-[200%]  opacity-0 z-20 bg-gradient-to-br from-cyan-300 to-pink-300 via-blue-300 font-proxima">Download PDF</Button> 
+                  </div>
+                  <Image id="resume" src="resume.png" className="border-0 group-hover:opacity-10 object-cover h-full w-full z-10" />
+                </CardBody>
+              </Card>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
---
+++ portfolio-v1/components/downloadsvg.jsx
@@ -0,0 +1,8 @@
+'use client'
+
+export default function DownloadSVG() {
+
+  return (
+    <img src="download.svg" className="w-[20px]" />
+  );
+}
